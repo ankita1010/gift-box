408055a05f6fd3c2bf109609e874b7fed9b499e7
Ankita Chakraborty <ankitachakraborty412@gmail.com>
2020-12-19 14:20:05 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import './App.scss';
+
+const App = () => {
+	return (
+		<div className="app-block">
+			<div className="big-wrapper">
+				<div className="cube-front">Front</div>
+				<div className="cube-back">Back</div>
+				<div className="cube-right">Right</div>
+				<div className="cube-left">Left</div>
+				<div className="cube-bottom">Bottom</div>
+				<div className="lid-wrapper">
+				<div className="lid-front">Front</div>
+				<div className="lid-back">Back</div>
+				<div className="lid-right">Right</div>
+				<div className="lid-left">Left</div>
+				<div className="lid-top">Top</div>
+			</div>
+			</div>
+			
+			
+		</div>
+	)
+};
+export default App;
