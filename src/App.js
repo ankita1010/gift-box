92bc18bd4023428fd896306ac211ce89d843c48c
Himayan <himayand21@gmail.com>
2020-12-20 05:18:11 +0530

    feat(ui): add a snowy christmas scene

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
 
+import {Lights} from "./Lights";
+import {Snowflakes} from "./Snowflakes";
+
+import snowfield from "./assets/snowfield.svg";
 import tree from "./assets/christmas-tree.svg";
+import moon from "./assets/moon.svg";
+import sleigh from "./assets/christmas-sleigh.svg";
 
 import "./App.scss";
 
@@ -8,75 +14,37 @@ const App = () => {
   return (
     <div className="app-body">
       <div className="background">
-        <ul class="lightrope">
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
-        <div class="wrapper">
-          <div class="road">
-            <div class="line"></div>
+		<Lights />
+		<Snowflakes />
+		<div className="snow-field">
+			<img src={snowfield} />
+		</div>
+        <div className="christmas-tree">
+          <img src={tree} />
+        </div>
+		<div className="moon">
+			<img src={moon} />
+		</div>
+		<div className="sleigh">
+			<img src={sleigh} />
+		</div>
+        <div className="box-container">
+          <div className="box-shadow" />
+          <div className="box-wrapper">
+            <div className="cube-face cube-front">Front</div>
+            <div className="cube-face cube-back">Back</div>
+            <div className="cube-face cube-right">Right</div>
+            <div className="cube-face cube-left">Left</div>
+            <div className="cube-face cube-bottom">Bottom</div>
+            <div className="lid-wrapper">
+              <div className="lid-face lid-front">Front</div>
+              <div className="lid-face lid-back">Back</div>
+              <div className="lid-face lid-right">Right</div>
+              <div className="lid-face lid-left">Left</div>
+              <div className="lid-face lid-top">Top</div>
+            </div>
           </div>
         </div>
-        <div className="christmas-tree-1">
-          <img src={tree} />
-        </div>
-		<div className="christmas-tree-2">
-          <img src={tree} />
-        </div>
-		<div className="box-container">
-			<div className="box-shadow" />
-				<div className="box-wrapper">
-					<div className="cube-face cube-front">Front</div>
-					<div className="cube-face cube-back">Back</div>
-					<div className="cube-face cube-right">Right</div>
-					<div className="cube-face cube-left">Left</div>
-					<div className="cube-face cube-bottom">Bottom</div>
-					<div className="lid-wrapper">
-						<div className="lid-face lid-front">Front</div>
-						<div className="lid-face lid-back">Back</div>
-						<div className="lid-face lid-right">Right</div>
-						<div className="lid-face lid-left">Left</div>
-						<div className="lid-face lid-top">Top</div>
-					</div>
-				</div>
-			</div>
       </div>
     </div>
   );
---
+++ src/Lights.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+export const Lights = () => {
+    return (
+        <ul className="lightrope">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+    )
+}
