37b7a6bce6236b8b4d42c564738aa085b3f33d16
Himayan <himayand21@gmail.com>
2020-12-20 03:39:01 +0530

    feat(ui): add christmas trees

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
+import React from "react";
 
-import './App.scss';
+import tree from "./assets/christmas-tree.svg";
+
+import "./App.scss";
 
 const App = () => {
-	return (
-		<div className="app-block">
+  return (
+    <div className="app-body">
+      <div className="background">
+        <ul class="lightrope">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+        <div class="wrapper">
+          <div class="road">
+            <div class="line"></div>
+          </div>
+        </div>
+        <div className="christmas-tree-1">
+          <img src={tree} />
+        </div>
+		<div className="christmas-tree-2">
+          <img src={tree} />
+        </div>
+		<div className="box-container">
 			<div className="box-shadow" />
-			<div className="box-wrapper">
-				<div className="cube-face cube-front">Front</div>
-				<div className="cube-face cube-back">Back</div>
-				<div className="cube-face cube-right">Right</div>
-				<div className="cube-face cube-left">Left</div>
-				<div className="cube-face cube-bottom">Bottom</div>
-				<div className="lid-wrapper">
-					<div className="lid-face lid-front">Front</div>
-					<div className="lid-face lid-back">Back</div>
-					<div className="lid-face lid-right">Right</div>
-					<div className="lid-face lid-left">Left</div>
-					<div className="lid-face lid-top">Top</div>
+				<div className="box-wrapper">
+					<div className="cube-face cube-front">Front</div>
+					<div className="cube-face cube-back">Back</div>
+					<div className="cube-face cube-right">Right</div>
+					<div className="cube-face cube-left">Left</div>
+					<div className="cube-face cube-bottom">Bottom</div>
+					<div className="lid-wrapper">
+						<div className="lid-face lid-front">Front</div>
+						<div className="lid-face lid-back">Back</div>
+						<div className="lid-face lid-right">Right</div>
+						<div className="lid-face lid-left">Left</div>
+						<div className="lid-face lid-top">Top</div>
+					</div>
 				</div>
 			</div>
-		</div>
-	)
+      </div>
+    </div>
+  );
 };
 export default App;
