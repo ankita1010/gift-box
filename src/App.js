5e668c29b5333ea2e11da226717c902d4c99c29a
Himayan <himayand21@gmail.com>
2020-12-19 23:01:32 +0530

    feat(ui): code cleanup

--- src/App.js
+++ src/App.js
@@ -6,22 +6,20 @@ const App = () => {
 	return (
 		<div className="app-block">
 			<div className="box-shadow" />
-			<div className="big-wrapper">
+			<div className="box-wrapper">
 				<div className="cube-face cube-front">Front</div>
 				<div className="cube-face cube-back">Back</div>
 				<div className="cube-face cube-right">Right</div>
 				<div className="cube-face cube-left">Left</div>
 				<div className="cube-face cube-bottom">Bottom</div>
 				<div className="lid-wrapper">
-				<div className="lid-front">Front</div>
-				<div className="lid-back">Back</div>
-				<div className="lid-right">Right</div>
-				<div className="lid-left">Left</div>
-				<div className="lid-top">Top</div>
+					<div className="lid-face lid-front">Front</div>
+					<div className="lid-face lid-back">Back</div>
+					<div className="lid-face lid-right">Right</div>
+					<div className="lid-face lid-left">Left</div>
+					<div className="lid-face lid-top">Top</div>
+				</div>
 			</div>
-			</div>
-			
-			
 		</div>
 	)
 };
