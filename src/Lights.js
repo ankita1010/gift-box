2876c8e8f5d46bdd73f36f08c4eaacc73d95d2b8
Ankita Chakraborty <ankitachakraborty412@gmail.com>
2020-12-22 21:05:36 +0530

    fix: using opacity animation instead of box-shadow to avoid paint flash

--- src/Lights.js
+++ src/Lights.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 export const Lights = () => {
-    return (
-        <ul className="lightrope">
-          {[...Array(40)].map((each, index) => <li key={`light-${index}`} />)}
-        </ul>
-    )
+  return (
+    <ul className="lightrope">
+      {[...Array(40)].map(
+        (each, index) =>
+          <li key={`light-${index}`}>
+            <div className="glow"></div>
+          </li>
+      )}
+    </ul>
+  )
 }
